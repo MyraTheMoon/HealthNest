791083c44020119ed77e088c3bfe2152df544636
MyraTheMoon <vishalharinkhede2008@gmail.com>
2026-05-17 01:33:28 +0530

    Update VANIE.js

--- VANIE.js
+++ VANIE.js
@@ -5,7 +5,7 @@
  * Version: 2.0 - Unified System
  */ 
 
-class VANIEAlgorithm {
+class VANIEAlgorithm { 
     constructor() {
         this.name = "VANIE - Virtual Assistant for Nursing & Health Information & Education";
         this.version = "2.0";
